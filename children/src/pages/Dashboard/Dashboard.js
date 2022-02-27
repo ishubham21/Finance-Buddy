90f57347e5b14b3e261e4f8d972e1c2e903d75f1
khush2706 <kc220313@gmail.com>
2022-02-27 13:27:13 +0530

    dashboard routing

--- children/src/pages/Dashboard/Dashboard.js
+++ children/src/pages/Dashboard/Dashboard.js
@@ -1,4 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Dashboard.css";
-const Dashboard = () => <h1>Dashboard Page</h1>;
+import { useHistory } from "react-router-dom";
+
+const Dashboard = () => {
+    const [dashboard, setDashboard] = useState(null);
+    const history = useHistory();
+    console.log(localStorage.getItem("token"));
+    const requestOptions = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            "auth-token": localStorage.getItem("token"),
+        },
+    };
+
+    useEffect(() => {
+        fetch(
+            "https://finance-buddy-backend.vercel.app/child/dashboard",
+            requestOptions
+        )
+        .then((response) => response.json()) // convert to json
+        .then(data => console.log(data))
+        .catch((err) => {
+            console.log(err);
+          }); // Catch errors
+    }, []);
+
+    return <h1>Dashboard</h1>;
+};
+
 export default Dashboard;
